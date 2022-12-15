50ae79108ff2967adf7f39d4d91bdef8f2cfb7bf
pedroafonsodias <pedroafonsodias@gmail.com>
2022-12-15 14:33:36 +0000

    3 files modified

--- src/components/AnsButton.ts
+++ src/components/AnsButton.ts
@@ -8,11 +8,32 @@ export class AnsButton extends LitElement {
   kind: string;
 
   @property()
-  icon: string = html`<svg style="color: white" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16"> <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" fill="white"></path> </svg>`;
+  size: string;
+
+  @property()
+  disabled: boolean;
+
+  @property()
+  icon: string;
+
+  constructor() {
+    super();
+    this.kind = 'primary';
+    this.disabled = false;
+    this.icon = '';
+    this.size = 'sm';
+  }
+
+  private getIcon() {
+    return this.icon == 'plus'
+      ? html`<svg style="color: white" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16"> <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" fill="white"></path> </svg>`
+      : '';
+  }
 
   render() {
-    return html`<bx-btn size="sm" kind=${this.kind}>
-    ${this.icon}
+    return html`<bx-btn size=${this.size} kind=${this.kind} 
+    ${this.disabled ? 'disabled' : ''}>
+    ${this.getIcon()}
     ${this.innerHTML}
     </bx-btn>`;
   }
--- src/pages/BookableEvents.ts
+++ src/pages/BookableEvents.ts
@@ -6,18 +6,42 @@ import '../components/AnsButton';
 export class BookableEventsPage extends LitElement {
   render() {
     return html`
-    <div id="header"></div>
-    <div id="content"></div>
-    
-    <ans-button kind="primary">Event</ans-button>
-    <p>This is the bookable events page</p>`;
+    <div id="header">
+    <table border='0'>
+      <tr>
+        <td>
+          <ans-button kind="secondary" size="sm" disabled="true">Link Sub-Environments</ans-button>
+        </td>
+        <td>
+          <ans-button kind="secondary" disabled="false">Filter Sub-Environments</ans-button>
+        </td>
+      </tr>
+    </table>
+    </div>
+    <div id="content">
+      <ans-button kind="primary" size="m" icon="plus">Event</ans-button>
+      <p>This is the bookable events page</p>
+    </div>`;
   }
 
   static styles = css`
   #header {
+    position: absolute;
+    top:0px;
+    left:0px;
     width: 100%;
-    height: 200px;
+    height: 100px;
     background-color: gray;
+  }
+
+  #content {
+    position:absolute;
+    top:100px;
+    left:0px;
+    margin-bottom: 0px;
+    width: 100%;
+    height: inherit;
+    background-color: white;
   }
   `;
 }
